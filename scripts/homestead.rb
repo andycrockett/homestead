48e3a8e75f6c47b18ad902765f5ddea7e6d881c1
Andy Crockett <acrockett@drctinc.com>
2014-06-15 19:47:36 -0500

    added support for hostmanager

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -1,86 +1,89 @@
 class Homestead
   def Homestead.configure(config, settings)
-    # Configure The Box
-    config.vm.box = "laravel/homestead"
-    config.vm.hostname = "homestead"
-
-    # Configure A Private Network IP
-    config.vm.network :private_network, ip: settings["ip"] ||= "192.168.10.10"
 
     # Configure hostmanager
     config.hostmanager.enabled = true
     config.hostmanager.manage_host = true # requires password during setup if true
     config.hostmanager.ignore_private_ip = false # visit your sites without specifying the port (:8000)
     config.hostmanager.include_offline = true
-    
-    # Define aliases variable so we can pass it to hostmanager after our nginx sites are set up
-    aliases = ""
 
-    # Configure A Few VirtualBox Settings
-    config.vm.provider "virtualbox" do |vb|
-      vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
-      vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
-      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
-      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
-    end
+    config.vm.define "homestead" do |node|
 
-    # Configure Port Forwarding To The Box
-    config.vm.network "forwarded_port", guest: 80, host: 8000
-    config.vm.network "forwarded_port", guest: 3306, host: 33060
-    config.vm.network "forwarded_port", guest: 5432, host: 54320
+      # Configure The Box
+      node.vm.box = "laravel/homestead"
+      node.vm.hostname = "homestead"
 
-    # Configure The Public Key For SSH Access
-    config.vm.provision "shell" do |s|
-      s.inline = "echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
-      s.args = [File.read(File.expand_path(settings["authorize"]))]
-    end
+      # Configure A Private Network IP
+      node.vm.network :private_network, ip: settings["ip"] ||= "192.168.10.10"
 
-    # Copy The SSH Private Keys To The Box
-    settings["keys"].each do |key|
-      config.vm.provision "shell" do |s|
-        s.privileged = false
-        s.inline = "echo \"$1\" > /home/vagrant/.ssh/$2 && chmod 600 /home/vagrant/.ssh/$2"
-        s.args = [File.read(File.expand_path(key)), key.split('/').last]
+      # Define aliases variable to pass to hostmanager later
+      aliases = ""
+
+      # Configure A Few VirtualBox Settings
+      node.vm.provider "virtualbox" do |vb|
+        vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
+        vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
+        vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+        vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
       end
-    end
 
-    # Copy The Bash Aliases
-    config.vm.provision "shell" do |s|
-      s.inline = "cp /vagrant/aliases /home/vagrant/.bash_aliases"
-    end
+      # Configure Port Forwarding To The Box
+      node.vm.network "forwarded_port", guest: 80, host: 8000
+      node.vm.network "forwarded_port", guest: 3306, host: 33060
+      node.vm.network "forwarded_port", guest: 5432, host: 54320
 
-    # Register All Of The Configured Shared Folders
-    settings["folders"].each do |folder|
-      config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil
-    end
-
-
-    # Install All The Configured Nginx Sites
-    settings["sites"].each do |site|
-      config.vm.provision "shell" do |s|
-          s.inline = "bash /vagrant/scripts/serve.sh $1 \"$2\" $3"
-          s.args = [site["name"], site['map'], site["to"]]
+      # Configure The Public Key For SSH Access
+      node.vm.provision "shell" do |s|
+        s.inline = "echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
+        s.args = [File.read(File.expand_path(settings["authorize"]))]
       end
-      # record site mappings for hostmanager
-      aliases << " " + site["map"]
-    end
 
-    # Configure All Of The Server Environment Variables
-    if settings.has_key?("variables")
-      settings["variables"].each do |var|
-        config.vm.provision "shell" do |s|
-            s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf && service php5-fpm restart"
-            s.args = [var["key"], var["value"]]
+      # Copy The SSH Private Keys To The Box
+      settings["keys"].each do |key|
+        node.vm.provision "shell" do |s|
+          s.privileged = false
+          s.inline = "echo \"$1\" > /home/vagrant/.ssh/$2 && chmod 600 /home/vagrant/.ssh/$2"
+          s.args = [File.read(File.expand_path(key)), key.split('/').last]
         end
       end
+
+      # Copy The Bash Aliases
+      node.vm.provision "shell" do |s|
+        s.inline = "cp /vagrant/aliases /home/vagrant/.bash_aliases"
+      end
+
+      # Register All Of The Configured Shared Folders
+      settings["folders"].each do |folder|
+        node.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil
+      end
+
+
+      # Install All The Configured Nginx Sites
+      settings["sites"].each do |site|
+        node.vm.provision "shell" do |s|
+            s.inline = "bash /vagrant/scripts/serve.sh $1 \"$2\" $3"
+            s.args = [site["name"], site['map'], site["to"]]
+        end
+        # record all site mappings for hostmanager
+        aliases << " " + site["map"]
+      end
+
+      # Configure All Of The Server Environment Variables
+      if settings.has_key?("variables")
+        settings["variables"].each do |var|
+          node.vm.provision "shell" do |s|
+              s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf && service php5-fpm restart"
+              s.args = [var["key"], var["value"]]
+          end
+        end
+      end
+
+      # pass aliases to hostmanagers
+      node.hostmanager.aliases = aliases
+      
+      # ensure hostmanager is run
+      node.vm.provision :hostmanager
     end
-
-    # pass aliases to hostmanagers
-    config.hostmanager.aliases = aliases
-
-    # ensure hostmanager is run
-    config.vm.provision :hostmanager   
-
   end
 end
 
